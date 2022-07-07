0f34d6ab11a74b1975a6d3197597fcec47ed3ef2
韦希[公司版] <gongsi@haha.com>
2022-07-07 17:32:30 +0800

    更新lil7

--- keyboard/hbk-lil7/a/actionmap_common.h
+++ keyboard/hbk-lil7/a/actionmap_common.h
@@ -34,17 +34,10 @@ extern const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS];
  * 第二组为按键实际矩阵
  */
 #define ACTIONMAP_ENC( \
-	K00, K01, K02, K03,    \
-	K10, K11, K12, K13,    \
-	K20, K21, K22,         \
-	K30, K31, K32, K33,    \
-	K40, K42,              \
-	R0P, R0N, K44\
+	K00, K01, K02,	 	\
+	K10, K11, K12, K13	\
 ) { \
-	{ AC_##K00, AC_##K01, AC_##K02, AC_##K03,   AC_NO }, \
-	{ AC_##K10, AC_##K11, AC_##K12, AC_##K13,   AC_NO }, \
-	{ AC_##K20, AC_##K21, AC_##K22, AC_NO,      AC_NO }, \
-	{ AC_##K30, AC_##K31, AC_##K32, AC_##K33,   AC_NO }, \
-	{ AC_##K40, AC_##R0P, AC_##K42, AC_##R0N,   AC_##K44   }, \
+	{ AC_##K00, AC_##K01, AC_##K02,   AC_NO }, \
+	{ AC_##K10, AC_##K11, AC_##K12, AC_##K13 }, \
 }
 
--- keyboard/hbk-lil7/a/actionmap_plain.c
+++ keyboard/hbk-lil7/a/actionmap_plain.c
@@ -20,22 +20,40 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #include "actionmap_common.h"
 #include "keyboard_fn.h"
 #include "user_fn.h"
-
+#ifdef RGB_MATRIX_ENABLE
+#include "rgb_matrix.h"
+#include "rgb_matrix_types.h"
+led_config_t g_led_config = {
+    {
+        // RGB顺序
+        { 0   ,  1  ,  2  ,  NO_LED },
+		{ 3   ,  4  ,  5  ,    6    },
+    },
+    { 	// RGB物理位置
+        { 16, 64 }, { 16, 48 }, { 16, 32 },
+		{ 32,64 },{ 32, 48 },{ 32, 32 },{ 24, 16 },
+	 },
+    { 	// RGB功能标志
+        0xff , 0xff , 0xff , 
+		0xff , 0xff , 0xff , 0xff}
+};
+#endif
 /* 
  * 定义需使用的FN按键
  */
-#define AC_FN0	ACTION_LAYER_TAP(2, KC_NLCK)
-#define AC_FN1	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_0)
-#define AC_FN2	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_1)
-#define AC_FN3	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_2)
-#define AC_FN4	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_READV)
-#define AC_FN5	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_REBOND)
-#define AC_FN6	ACTION_FUNCTION_OPT(RGB_LIGHT_CONTROL, RGB_LIGHT_STEP)
-#define AC_FN7  ACTION_FUNCTION_OPT(RGB_LIGHT_CONTROL, RGB_LIGHT_TOGGLE)
-#define AC_FN8  ACTION_FUNCTION_OPT(RGB_LIGHT_CONTROL, RGB_LIGHT_TML)
-#define AC_FN9	ACTION_FUNCTION_OPT(RGB_LIGHT_CONTROL, RGB_LIGHT_IHUE)
-#define AC_FN10	ACTION_FUNCTION_OPT(RGB_LIGHT_CONTROL, RGB_LIGHT_DHUE)
-#define AC_FN11	ACTION_LAYER_TAP(1, KC_PDOT)
+#define AC_FN0	ACTION_LAYER_MOMENTARY(1)	//跳转1层
+#define AC_FN1	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_0)//蓝牙0
+#define AC_FN2	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_1)//蓝牙1
+#define AC_FN3	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_READV)//广播
+#define AC_FN4	ACTION_FUNCTION_OPT(SWITCH_DEVICE, SWITCH_DEVICE_BLE_REBOND)//解绑
+#define AC_FN5	ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_TOGGLE)//开关灯
+#define AC_FN6	ACTION_FUNCTION_OPT(USER_KEYBOARD_CONTROL, CONTROL_TOGGLE_INDICATOR_LIGHT)// 开关指示灯
+#define AC_FN7  ACTION_LAYER_MOMENTARY(2) //跳转2层
+#define AC_FN8  ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_MODE_INCREASE)//模式+
+#define AC_FN9	ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_VAL_INCREASE)//亮度+
+#define AC_FN10	ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_VAL_DECREASE)//亮度-
+#define AC_FN11	ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_HUE_INCREASE)//色相+
+#define AC_FN12	ACTION_FUNCTION_OPT(RGBMATRIX_CONTROL, RGBLIGHT_HUE_DECREASE)//色相-
 
 /* 
  * @brief 按键映射
@@ -45,26 +63,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 const action_t actionmaps[][MATRIX_ROWS][MATRIX_COLS] = { 
 	[0] = ACTIONMAP_ENC(
-		FN0,  PSLS,  PAST, PMNS,\
-		P7,   P8,    P9,   PPLS, \
-		P4,   P5,    P6,  \
-		P1,   P2,    P3,   PENT, \
-		P0,   FN11, \
-		VOLU, VOLD,  FN0 ),
+		Q,  W,  E, \
+		FN0,   C,    V,   PENT),
 
 	[1] = ACTIONMAP_ENC(
-		TRNS,  FN1,   FN2,  FN3,\
-		FN6,   FN7,   FN8,  FN4, \
-		MYCM,  MAIL,  MSEL,  \
-		MPRV,  MPLY,  MNXT, FN5, \
-		BSPC,  CALC, \
-		FN9,  FN10, TRNS),
+		FN7,  FN1,   FN2, \
+		NO,   NO,   FN4,  FN3),
 
 	[2] = ACTIONMAP_ENC(  //Android电视遥控器
-		NO,    NO,    VOLU, VOLD,\
-		NO,    UP,    NO,   HOME, \
-		LEFT,  ENTER, RIGHT,  \
-		NO,    DOWN,  NO,   ESC, \
-		APP,   NO, \
-		TRNS,  TRNS,  TRNS ),
+		NO,    FN12,   FN11, \
+		NO,    FN8,    FN6,   FN5),
 };
